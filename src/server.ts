aed57a0bbb80770435bff7a698a3bb542f6a57cf
Jaedu Cho <chojd7@gmail.com>
2021-11-20 11:25:21 -0800

    change port 8082 to 8080

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080;
 
   //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   
