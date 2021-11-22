f0c843813568cdc770e78746fc9d2059e5ea92ac
Jaedu Cho <chojd7@gmail.com>
2021-11-22 09:43:34 -0800

    add error handler

--- src/server.ts
+++ src/server.ts
@@ -40,13 +40,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       return res.status(400).send('url is required.');
     }
     // 2. call filterImageFromURL(image_url) to filter the image
-    let filteredpath = await filterImageFromURL(image_url);
-    let local_image_list = [filteredpath];
+    filterImageFromURL(image_url).then((filteredpath) => {
+      if(filteredpath === "error") {
+        console.log("Can't read URL");
+        return res.status(415).send("Can't read URL");
+      } else {
+        console.log('success');
+        return res.status(200).sendFile(filteredpath, () => {deleteLocalFiles([filteredpath])});
+      }})
+    });
+    
+
+
+    // let filteredpath = await filterImageFromURL(image_url);
+    // let local_image_list = [filteredpath];
     // 3. send the resulting file in the response and
     // 4. delete any filtes on the server on finish of the response
-    return res.status(200).sendFile(filteredpath, () => {deleteLocalFiles(local_image_list)});
-  });
-
+    // return res.status(200).sendFile(filteredpath, () => {deleteLocalFiles(local_image_list)});
+  // });
   
   // Root Endpoint
   // Displays a simple message to the user
